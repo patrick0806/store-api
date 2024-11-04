bbc3199e399000ea9d5f2d7df8586e9713f95f8f
patrick0806 <patrickk0806@gmail.com>
2024-11-04 11:03:09 -0300

    chore: vitests coverage ignore enums

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
       exclude: [
         '**/*.entity.ts',
         '**/*.dto.ts',
+        '**/*.enum.ts',
         ...coverageConfigDefaults.exclude
       ]
     }
